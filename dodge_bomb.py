d835672648d2da077df6800543105b6aeff90796
RyugaIshii <c0a2200935@edu.teu.ac.jp>
2023-11-28 14:32:34 +0900

    練習こうかとんの移動

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,6 +5,12 @@ import pygame as pg
 
 WIDTH, HEIGHT = 1600, 800
 
+delta = {
+    pg.K_UP:(0,-5),
+    pg.K_DOWN:(0,5),
+    pg.K_LEFT:(-5,0),
+    pg.K_RIGHT:(5,0)
+}
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -12,6 +18,7 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rect = kk_img.get_rect()
     clock = pg.time.Clock()
     bb_bomb = pg.Surface((20,20))  #練習１
     pg.draw.circle(bb_bomb,(255,0,0),(10,10),10)  #練習１
@@ -19,7 +26,7 @@ def main():
     bb_bomb.set_colorkey((0,0,0)) #練習１
     bb_rect.centerx = random.randint(0,WIDTH)  #練習１
     bb_rect.centery = random.randint(0,HEIGHT)  #練習１
-    vx,vy = 5,1 #練習２
+    vx,vy = 5,5 #練習２
     
     tmr = 0
     while True:
@@ -27,8 +34,16 @@ def main():
             if event.type == pg.QUIT: 
                 return
 
+        key_lst = pg.key.get_pressed()
+        sum_move = [0,0]
+        for k,tpl in delta.items():
+            if key_lst[k]: 
+                sum_move[0] += tpl[0]
+                sum_move[1] += tpl[1]
+
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        kk_rect.move_ip(sum_move[0],sum_move[1])
+        screen.blit(kk_img, kk_rect)
         bb_rect.move_ip(vx,vy) #練習2　爆弾の移動
         screen.blit(bb_bomb,bb_rect)  #練習１
         pg.display.update()
