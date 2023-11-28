8cd0293fe6b9fdce1e99eba34a09f4f481882be7
RyugaIshii <c0a2200935@edu.teu.ac.jp>
2023-11-28 14:09:02 +0900

    爆弾Surfaceの追加

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import pygame as pg
 
@@ -12,6 +13,12 @@ def main():
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     clock = pg.time.Clock()
+    bb_bomb = pg.Surface((20,20))  #練習１
+    pg.draw.circle(bb_bomb,(255,0,0),(10,10),10)  #練習１
+    bb_rect = bb_bomb.get_rect()  #練習１
+    bb_bomb.set_colorkey((0,0,0)) #練習１
+    bb_rect.centerx = random.randint(0,WIDTH)  #練習１
+    bb_rect.centery = random.randint(0,HEIGHT)  #練習１
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -20,6 +27,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bb_bomb,bb_rect)
         pg.display.update()
         tmr += 1
         clock.tick(10)
